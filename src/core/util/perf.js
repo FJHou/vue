d67585e220f1a802241b0647650ea4c42d550ea3
houfujian <ApeLiftingIron@163.com>
2018-10-05 22:52:47 +0800

    学习了perf.js,window.perfomance

--- src/core/util/perf.js
+++ src/core/util/perf.js
@@ -1,3 +1,8 @@
+/**
+ * 这个文件主要是用来检测性能，具体文档可以查看MDN：
+ * https://developer.mozilla.org/zh-CN/docs
+ * /Web/API/Window/performance
+ */
 import { inBrowser } from './env'
 
 export let mark
@@ -13,6 +18,14 @@ if (process.env.NODE_ENV !== 'production') {
     perf.clearMarks &&
     perf.clearMeasures
   ) {
+    /**
+     * performance.mark()通过一个给定的名称，
+     * 将该名称（作为键）和对应的DOMHighResTimeStamp
+     *（作为值）保存在一个哈希结构里。该键值对表示了从某一时刻
+     *（译者注：某一时刻通常是 navigationStart 事件发生时刻）
+     * 到记录时刻间隔的毫秒数。（译者注：该方法一般用来多次记录时
+     * 间，用于求得各记录间的时间差）
+     */
     mark = tag => perf.mark(tag)
     measure = (name, startTag, endTag) => {
       perf.measure(name, startTag, endTag)
